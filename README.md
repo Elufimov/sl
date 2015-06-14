fc9df80f76d8cfca358993e537e9e992be099e51
Oleg Berman <bermanoleg@gmail.com>
2015-06-14 15:05:44 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-SL(1): Cure your bad habit of mistyping
-=======================================
+<h1>Doge Dogomotive</h1>
+<p>This is a silly fork of Steam Locomotive by Toyoda Masashi that replaces an animated train with a static doge. </p>
+<code>
+                Y.                      _           
+                YiL                   .```.         
+                Yii;                .; .;;`.        
+                YY;ii._           .;`.;;;; :        
+                iiYYYYYYiiiii;;;;i` ;;::;;;;        
+            _.;YYYYYYiiiiiiYYYii  .;;.   ;;;        
+         .YYYYYYYYYYiiYYYYYYYYYYYYii;`  ;;;;        
+       .YYYYYYY$$YYiiYY$$$$iiiYYYYYY;.ii;`..        
+      :YYY$!.  TYiiYY$$$$$YYYYYYYiiYYYYiYYii.       
+      Y$MM$:   :YYYYYY$!'``'4YYYYYiiiYYYYiiYY.      
+   `. :MM$$b.,dYY$$Yii' :'   :YYYYllYiiYYYiYY       
+_.._ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY       
+.,._ $b`P`     '4$$$$$iiiiiiii$$$$YY$$$$$$YiY;      
+   `,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL     
+    '`;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU:    
+    ;$P$;;: ;;;;i$y$'!Y$$$b;$$$Y$YY$$YYYiiiYYiYY    
+    $Fi$$ .. ``:iii.`-':YYYYY$$YY$$$$$YYYiiYiYYY    
+    :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYYiYY:    
+     :$$$$$i;;iiiiidYYYYYYYYYY$$$$$$YYYYYYYiiYYY.   
+      `$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY  
+      .i!$$$$$$YYYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY  
+     :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'  
+</code>
+<small>The ascii image of the dog was found somewhere on Reddit</small>
 
-SL (Steam Locomotive) runs across your terminal when you type "sl" as
-you meant to type "ls". It's just a joke command, and not useful at
-all.
-
-Copyright 1993,1998,2014 Toyoda Masashi (mtoyoda@acm.org)
+<gray>Please address to the original repo: </gray><a href="https://github.com/mtoyoda/sl">https://github.com/mtoyoda/sl</a>
--- sl.h
+++ sl.h
@@ -1,150 +1,29 @@
-/*========================================
- *    sl.h: SL version 5.02
- *	Copyright 1993,2002,2014
- *                Toyoda Masashi
- *		  (mtoyoda@acm.org)
- *	Last Modified: 2014/06/03
- *========================================
- */
+/* This is a silly modification 
+   of Toyoda Masashi's SL package
+   that removes the train and adds doge instead
+   Original github repo is at: 
+   https://github.com/mtoyoda/sl
+*/
 
-#define D51HIGHT	10
-#define D51FUNNEL	 7
-#define D51LENGTH	83
-#define D51PATTERNS	 6
-
-
-#define D51STR1  "      ====        ________                ___________ "
-#define D51STR2  "  _D _|  |_______/        \\__I_I_____===__|_________| "
-#define D51STR3  "   |(_)---  |   H\\________/ |   |        =|___ ___|   "
-#define D51STR4  "   /     |  |   H  |  |     |   |         ||_| |_||   "
-#define D51STR5  "  |      |  |   H  |__--------------------| [___] |   "
-#define D51STR6  "  | ________|___H__/__|_____/[][]~\\_______|       |   "
-#define D51STR7  "  |/ |   |-----------I_____I [][] []  D   |=======|__ "
-
-#define D51WHL11 "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ "
-#define D51WHL12 " |/-=|___|=    ||    ||    ||    |_____/~\\___/        "
-#define D51WHL13 "  \\_/      \\O=====O=====O=====O_/      \\_/            "
-
-#define D51WHL21 "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ "
-#define D51WHL22 " |/-=|___|=O=====O=====O=====O   |_____/~\\___/        "
-#define D51WHL23 "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            "
-
-#define D51WHL31 "__/ =| o |=-O=====O=====O=====O \\ ____Y___________|__ "
-#define D51WHL32 " |/-=|___|=    ||    ||    ||    |_____/~\\___/        "
-#define D51WHL33 "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            "
-
-#define D51WHL41 "__/ =| o |=-~O=====O=====O=====O\\ ____Y___________|__ "
-#define D51WHL42 " |/-=|___|=    ||    ||    ||    |_____/~\\___/        "
-#define D51WHL43 "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            "
-
-#define D51WHL51 "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ "
-#define D51WHL52 " |/-=|___|=   O=====O=====O=====O|_____/~\\___/        "
-#define D51WHL53 "  \\_/      \\__/  \\__/  \\__/  \\__/      \\_/            "
-
-#define D51WHL61 "__/ =| o |=-~~\\  /~~\\  /~~\\  /~~\\ ____Y___________|__ "
-#define D51WHL62 " |/-=|___|=    ||    ||    ||    |_____/~\\___/        "
-#define D51WHL63 "  \\_/      \\_O=====O=====O=====O/      \\_/            "
-
-#define D51DEL   "                                                      "
-
-#define COAL01 "                              "
-#define COAL02 "                              "
-#define COAL03 "    _________________         "
-#define COAL04 "   _|                \\_____A  "
-#define COAL05 " =|                        |  "
-#define COAL06 " -|                        |  "
-#define COAL07 "__|________________________|_ "
-#define COAL08 "|__________________________|_ "
-#define COAL09 "   |_D__D__D_|  |_D__D__D_|   "
-#define COAL10 "    \\_/   \\_/    \\_/   \\_/    "
-
-#define COALDEL "                              "
-
-#define LOGOHIGHT    	 6
-#define LOGOFUNNEL  	 4
-#define LOGOLENGTH      84
-#define LOGOPATTERNS	 6
-
-#define LOGO1  "     ++      +------ "
-#define LOGO2  "     ||      |+-+ |  "
-#define LOGO3  "   /---------|| | |  "
-#define LOGO4  "  + ========  +-+ |  "
-
-#define LWHL11 " _|--O========O~\\-+  "
-#define LWHL12 "//// \\_/      \\_/    "
-
-#define LWHL21 " _|--/O========O\\-+  "
-#define LWHL22 "//// \\_/      \\_/    "
-
-#define LWHL31 " _|--/~O========O-+  "
-#define LWHL32 "//// \\_/      \\_/    "
-
-#define LWHL41 " _|--/~\\------/~\\-+  "
-#define LWHL42 "//// \\_O========O    "
-
-#define LWHL51 " _|--/~\\------/~\\-+  "
-#define LWHL52 "//// \\O========O/    "
-
-#define LWHL61 " _|--/~\\------/~\\-+  "
-#define LWHL62 "//// O========O_/    "
-
-#define LCOAL1 "____                 "
-#define LCOAL2 "|   \\@@@@@@@@@@@     "
-#define LCOAL3 "|    \\@@@@@@@@@@@@@_ "
-#define LCOAL4 "|                  | "
-#define LCOAL5 "|__________________| "
-#define LCOAL6 "   (O)       (O)     "
-
-#define LCAR1  "____________________ "
-#define LCAR2  "|  ___ ___ ___ ___ | "
-#define LCAR3  "|  |_| |_| |_| |_| | "
-#define LCAR4  "|__________________| "
-#define LCAR5  "|__________________| "
-#define LCAR6  "   (O)        (O)    "
-
-#define DELLN  "                     "
-
-#define C51HIGHT 11
-#define C51FUNNEL 7
-#define C51LENGTH 87
-#define C51PATTERNS 6
-
-#define C51DEL "                                                       "
-
-#define C51STR1 "        ___                                            "
-#define C51STR2 "       _|_|_  _     __       __             ___________"
-#define C51STR3 "    D__/   \\_(_)___|  |__H__|  |_____I_Ii_()|_________|"
-#define C51STR4 "     | `---'   |:: `--'  H  `--'         |  |___ ___|  "
-#define C51STR5 "    +|~~~~~~~~++::~~~~~~~H~~+=====+~~~~~~|~~||_| |_||  "
-#define C51STR6 "    ||        | ::       H  +=====+      |  |::  ...|  "
-#define C51STR7 "|    | _______|_::-----------------[][]-----|       |  "
-
-#define C51WH61 "| /~~ ||   |-----/~~~~\\  /[I_____I][][] --|||_______|__"
-#define C51WH62 "------'|oOo|==[]=-     ||      ||      |  ||=======_|__"
-#define C51WH63 "/~\\____|___|/~\\_|   O=======O=======O  |__|+-/~\\_|     "
-#define C51WH64 "\\_/         \\_/  \\____/  \\____/  \\____/      \\_/       "
-
-#define C51WH51 "| /~~ ||   |-----/~~~~\\  /[I_____I][][] --|||_______|__"
-#define C51WH52 "------'|oOo|===[]=-    ||      ||      |  ||=======_|__"
-#define C51WH53 "/~\\____|___|/~\\_|    O=======O=======O |__|+-/~\\_|     "
-#define C51WH54 "\\_/         \\_/  \\____/  \\____/  \\____/      \\_/       "
-
-#define C51WH41 "| /~~ ||   |-----/~~~~\\  /[I_____I][][] --|||_______|__"
-#define C51WH42 "------'|oOo|===[]=- O=======O=======O  |  ||=======_|__"
-#define C51WH43 "/~\\____|___|/~\\_|      ||      ||      |__|+-/~\\_|     "
-#define C51WH44 "\\_/         \\_/  \\____/  \\____/  \\____/      \\_/       "
-
-#define C51WH31 "| /~~ ||   |-----/~~~~\\  /[I_____I][][] --|||_______|__"
-#define C51WH32 "------'|oOo|==[]=- O=======O=======O   |  ||=======_|__"
-#define C51WH33 "/~\\____|___|/~\\_|      ||      ||      |__|+-/~\\_|     "
-#define C51WH34 "\\_/         \\_/  \\____/  \\____/  \\____/      \\_/       "
-
-#define C51WH21 "| /~~ ||   |-----/~~~~\\  /[I_____I][][] --|||_______|__"
-#define C51WH22 "------'|oOo|=[]=- O=======O=======O    |  ||=======_|__"
-#define C51WH23 "/~\\____|___|/~\\_|      ||      ||      |__|+-/~\\_|     "
-#define C51WH24 "\\_/         \\_/  \\____/  \\____/  \\____/      \\_/       "
-
-#define C51WH11 "| /~~ ||   |-----/~~~~\\  /[I_____I][][] --|||_______|__"
-#define C51WH12 "------'|oOo|=[]=-      ||      ||      |  ||=======_|__"
-#define C51WH13 "/~\\____|___|/~\\_|  O=======O=======O   |__|+-/~\\_|     "
-#define C51WH14 "\\_/         \\_/  \\____/  \\____/  \\____/      \\_/       "
+#define DOGESTR1  "                Y.                      _           "
+#define DOGESTR2  "                YiL                   .```.         "
+#define DOGESTR3  "                Yii;                .; .;;`.        "
+#define DOGESTR4  "                YY;ii._           .;`.;;;; :        "
+#define DOGESTR5  "                iiYYYYYYiiiii;;;;i` ;;::;;;;        "
+#define DOGESTR6  "            _.;YYYYYYiiiiiiYYYii  .;;.   ;;;        "
+#define DOGESTR7  "         .YYYYYYYYYYiiYYYYYYYYYYYYii;`  ;;;;        "
+#define DOGESTR8  "       .YYYYYYY$$YYiiYY$$$$iiiYYYYYY;.ii;`..        "
+#define DOGESTR9  "      :YYY$!.  TYiiYY$$$$$YYYYYYYiiYYYYiYYii.       "
+#define DOGESTR10 "      Y$MM$:   :YYYYYY$!'``'4YYYYYiiiYYYYiiYY.      "
+#define DOGESTR11 "   `. :MM$$b.,dYY$$Yii' :'   :YYYYllYiiYYYiYY       "
+#define DOGESTR12 "_.._ :`4MM$!YYYYYYYYYii,.__.diii$$YYYYYYYYYYY       "
+#define DOGESTR13 ".,._ $b`P`     '4$$$$$iiiiiiii$$$$YY$$$$$$YiY;      "
+#define DOGESTR14 "   `,.`$:       :$$$$$$$$$YYYYY$$$$$$$$$YYiiYYL     "
+#define DOGESTR15 "    '`;$$.    .;PPb$`.,.``T$$YY$$$$YYYYYYiiiYYU:    "
+#define DOGESTR16 "    ;$P$;;: ;;;;i$y$'!Y$$$b;$$$Y$YY$$YYYiiiYYiYY    "
+#define DOGESTR17 "    $Fi$$ .. ``:iii.`-':YYYYY$$YY$$$$$YYYiiYiYYY    "
+#define DOGESTR18 "    :Y$$rb ````  `_..;;i;YYY$YY$$$$$$$YYYYYYYiYY:   " 
+#define DOGESTR19 "     :$$$$$i;;iiiiidYYYYYYYYYY$$$$$$YYYYYYYiiYYY.   "
+#define DOGESTR20 "      `$$$$$$$YYYYYYYYYYYYY$$$$$$YYYYYYYYiiiYYYYYY  "
+#define DOGESTR21 "      .i!$$$$$$YYYYYYYYY$$$$$$YYY$$YYiiiiiiYYYYYYY  "
+#define DOGESTR22 "     :YYiii$$$$$$$YYYYYYY$$$$YY$$$$YYiiiiiYYYYYYi'  "
